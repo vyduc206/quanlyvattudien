b89d188ccf320aab500286cb8f99a2121ea33ce4
vyduc206 <quyduc.brland@gmail.com>
2025-04-19 16:47:37 +0700

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -6,4 +6,4 @@ from routes.xuat_kho import bp as xuat_kho_bp
 from routes.nha_cung_cap import bp as nha_cung_cap_bp
 from routes.danh_muc import bp as danh_muc_bp 
 from routes.bao_cao import bp as bao_cao_bp
-from routes.hang_san_xuat import bp as hang_san_xuat_bp
+
