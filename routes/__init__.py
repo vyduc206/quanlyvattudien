194c43b9c3a72ed2b3e7732177ab9f31bd7e58b8
vyduc206 <quyduc.brland@gmail.com>
2025-04-19 16:26:54 +0700

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,20 +1,9 @@
-from flask import Blueprint
-from app import app
-from routes.auth import auth_bp
-from routes.san_pham import san_pham_bp
-from routes.danh_muc import danh_muc_bp
-from routes.nha_cung_cap import nha_cung_cap_bp
-from routes.nhap_kho import nhap_kho_bp
-from routes.xuat_kho import xuat_kho_bp
-from routes.bao_cao import bao_cao_bp
-from routes.hang_san_xuat import hang_san_xuat_bp
 
-# Đăng ký các blueprint
-app.register_blueprint(auth_bp)
-app.register_blueprint(san_pham_bp)
-app.register_blueprint(danh_muc_bp)
-app.register_blueprint(nha_cung_cap_bp)
-app.register_blueprint(nhap_kho_bp)
-app.register_blueprint(xuat_kho_bp)
-app.register_blueprint(bao_cao_bp)
-app.register_blueprint(hang_san_xuat_bp)
+from routes.auth import bp as auth_bp
+from routes.san_pham import bp as san_pham_bp
+from routes.nhap_kho import bp as nhap_kho_bp  
+from routes.xuat_kho import bp as xuat_kho_bp
+from routes.nha_cung_cap import bp as nha_cung_cap_bp
+from routes.danh_muc import bp as danh_muc_bp 
+from routes.bao_cao import bp as bao_cao_bp
+from routes.hang_san_xuat import bp as hang_san_xuat_bp
